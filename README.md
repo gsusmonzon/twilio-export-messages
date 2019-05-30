7df9ffa1b705b454566d700a6458bd0f4b5a24e6
Jesus Monzon <jesus@microhealth.org>
2019-05-30 12:32:16 +0200

    Fix utf-8 encoding problems

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ python script to export twilio message logs en masse
 - Input account_sid and auth_pass with your credentials. Check Twilio API Explorer if unsure.
 - To run the script in terminal: `python twilio.py`
 
-Known issue: if inbound messages contain emojis or strange characters the csv writing throws an error. Fixed this by filtering to only write outbound messages. If you have a fix please PR it.
+Changelog:
+
+- 2019-05-30 Cope with unicode encoding
+- 2019-05-30 Changed to write to csv as data is fetch (avoid to keep huge list in memory)
+
+~~Known issue: if inbound messages contain emojis or strange characters the csv writing throws an error. Fixed this by filtering to only write outbound messages. If you have a fix please PR it.~~
--- twilio.py
+++ twilio.py
@@ -1,56 +1,49 @@
 import csv
 import requests
+import codecs
 
 account_sid = ""
 auth_pass = ""
 
 results_per_page = 1000
-num_pages = 500
+num_pages = 1000
 start_date = "2010-04-01"
 
+page = 0
 base_url = "https://api.twilio.com"
 initial_page = "/" + start_date + "/Accounts/" + account_sid + "/Messages.json?PageSize=" + str(results_per_page)
+next_page = None
+write_header = True
 
-response = requests.get(base_url + initial_page, auth=(account_sid, auth_pass)).json()
-next_page = response['next_page_uri']
-
-page = 0
-data = response['messages']
-
-# print response['previous_page_uri']
-# print response
-
-if not next_page == None:
-  while page < num_pages:
-    response = requests.get(base_url + next_page, auth=(account_sid, auth_pass)).json()
-
-    next_page = response['next_page_uri']
-
-    print "On page: " + str(page)
-
-    data = data + response['messages']
-    page += 1
-
-    if next_page == None:
-      print "No more pages"
-      break
-
-csv_file = open('export.csv', 'w')
+csv_file = codecs.open('export.csv', 'w')
 csvwriter = csv.writer(csv_file)
 
-count = 0
+while page < num_pages:
 
-for message in data:
+  if page == 0:
+    response = requests.get(base_url + initial_page, auth=(account_sid, auth_pass)).json()
+  else:
+    response = requests.get(base_url + next_page, auth=(account_sid, auth_pass)).json()
 
-  if count == 0:
-    header = message.keys()
-    csvwriter.writerow(header)
-    count += 1
+  next_page = response['next_page_uri']
+  data = response['messages']
 
-  #only include outbound sms because inbound ascii/emojis throws csv writer errors
-  if message.values()[2] == "outbound-api":
-    csvwriter.writerow(message.values())
+  print("On page: " + str(page))
+  page += 1
+
+  for message in data:
+
+    if write_header:
+      header = message.keys()
+      csvwriter.writerow(header)
+      write_header = False
+
+    csvwriter.writerow([s.encode('utf-8') if type(s) is unicode else s for s in message.values()])
+
+
+  if next_page == None:
+    print("No more pages")
+    break
 
 csv_file.close()
-
-print "Done!"
+print("Done!")
